a2b371d64052e4b82cbeec4794956e039d2e2017
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-01-12 21:45:42 -0500

    scanner lexeme recognition of some token types

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -27,7 +27,7 @@ class Scanner {
             scanToken();
         }
 
-        tokens.add(newToken(EOF, "", null, line));
+        tokens.add(new Token(EOF, "", null, line));
         return tokens;
     }
 
@@ -35,5 +35,83 @@ class Scanner {
         return current >= source.length();
     }
 
+    private void scanToken() {
+        char c = advance();
+        switch(c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL: EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+
+            case '/':
+                if(match('/')){
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                }else {
+                    addToken(SLASH);
+                }
+                break;
+
+            case ' ':
+            case '\r':
+            case '\t':
+              // Ignore whitespace.
+              break;
+
+            case '\n':
+              line++;
+              break;
+
+            default:
+              Lox.error(line, "Unexpected character.");
+              break;
+        }
+    }
+
+    private char advance(){
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type){
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected){
+        if(isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private char peek() {
+          if (isAtEnd()) return '\0';
+          return source.charAt(current);
+    }
 }
 
