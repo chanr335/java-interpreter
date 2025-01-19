a6b5ac8ab921a9ed1894cdbc9c85e0d91b05207b
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-01-19 14:52:00 -0500

    Finished Scanner; words/identifiers added

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -11,6 +11,7 @@ import static com.craftinginterpreters.lox.TokenType.*;
 class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private static final Map<String, TokenType> keywords;
 
     private int start = 0; //points to first character in lexeme being scanned
     private int current = 0; //tracks what source line current is on
@@ -86,11 +87,34 @@ class Scanner {
             default:
             if (isDigit(c)){
                 number();
+            } else if (isAlpha(c)){
+                identifier();
             } else{
               Lox.error(line, "Unexpected character.");
             }
             break;
         }
+    }
+
+
+    static{
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
     }
 
     private char advance(){
@@ -158,5 +182,25 @@ class Scanner {
     private char peekNext() {
         if(current + 1 >= source.length()) return '\0';
         return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c){
+        return (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+            c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c){
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private void identifier(){
+        while(isAlphaNumeric(peek())) advance();
+
+        //check if the identifier matches anything in the map
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null) type = IDENTIFIER;
+        addToken(type);
     }
 }
