7bea641cd43af5462726967f75aa0a8c297477fa
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-01-24 01:00:39 -0500

    finished evaluating valid expressions

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -21,6 +21,19 @@ class Interpreter implements Expr.Visitor<Object>{
         return null;
     }
 
+    private boolean isTruthy(Object object){
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b){
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+
+        return a.equals(b);
+    }
+
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr){
         return evaluate(expr.expression);
@@ -29,5 +42,40 @@ class Interpreter implements Expr.Visitor<Object>{
 
     private Object evaluate(Expr expr){
         return expr.accept(this);
+    }
+
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr){
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type){
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL: 
+                return (double)left <= (double)right;
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double){
+                    return (double)left + (double)right;
+                }
+                if (left instanceof String && right instanceof String){
+                    return (String)left + (String)right;
+                }
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+            case BANG_EQUAL: return !isEqual(left, right);
+            case EQUAL_EQUAL: return isEqual(left, right);
+        }
+        return null;
     }
 }
