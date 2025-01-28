45aead6924a2b53a5818298e3289dfe2b0472af8
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-01-28 00:37:20 -0500

    Variable mapping/Environment Added

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,7 +1,9 @@
-import java.util.List;
 package com.craftinginterpreters.lox;
+import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+    private Environment environment = new Environment();
 
     void interpret(List<Stmt> statements){
         try{
@@ -34,6 +36,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr){
+        return environment.get(expr.name);
     }
 
     private boolean isTruthy(Object object){
@@ -86,6 +93,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     public Void visitPrintStmt(Stmt.Print stmt){
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt){
+        Object value = null;
+        if (stmt.initializer != null){
+            value = evaluate(stmt.intializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
         return null;
     }
 
