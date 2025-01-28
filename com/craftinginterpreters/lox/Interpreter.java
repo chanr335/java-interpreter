aa6c45c63a5ba58fc34a9f64a3881fe75ee6ea04
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-01-28 00:57:24 -0500

    Implmented variable assignment

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -100,11 +100,18 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     public Void visitVarStmt(Stmt.Var stmt){
         Object value = null;
         if (stmt.initializer != null){
-            value = evaluate(stmt.intializer);
+            value = evaluate(stmt.initializer);
         }
 
         environment.define(stmt.name.lexeme, value);
         return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr){
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
     }
 
 
--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -32,9 +32,10 @@ class Parser {
         return statements;
     }
 
-    //expression -> equality;
+    //expression -> equality;.....
+    //expression -> assignment ;
     private Expr expression(){
-        return equality();
+        return assignment();
     }
     
     //delcaration -> varDecl | statement;
@@ -79,6 +80,24 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    //assignment -> IDENTIFIER "=" assignment | equality ;
+    private Expr assignment(){
+        Expr expr = equality();
+
+        if (match(EQUAL)){
+            Token equals = previous();
+            Expr value = assignment();
+
+            if (expr instanceof Expr.Variable){
+                Token name = ((Expr.Variable)expr).name;
+                return new Expr.Assign(name, value);
+            }
+
+            error(equals, "Invalid assignment target.");
+        }
+        return expr;
     }
 
     //equality -> comparison(( "!=" | "==") comparison)*;
