27903c1e3325ec50c64e2c479a506b12bb33c873
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-01-29 01:21:49 -0500

    Implemented scope

--- com/craftinginterpreters/lox/Environment.java
+++ com/craftinginterpreters/lox/Environment.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
 
     Object get(Token name){
         if(values.containsKey(name.lexeme)){
             return values.get(name.lexeme);
         }
+        if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name,
             "Undefined variable '" + name.lexeme + "'.");
@@ -21,11 +23,27 @@ class Environment {
             return;
         }
 
+        //check for variable in outer scopes
+        if (enclosing != null){
+            enclosing.assign(name, value);
+            return;
+        }
+
         throw new RuntimeError(name,
             "Undefined variable '" + name.lexeme + "'.");
     }
 
     void define(String name, Object value){
         values.put(name, value);
+    }
+
+    //global scope/ end of chain environment
+    Environment(){
+        enclosing = null;
+    }
+
+    //nested scopes
+    Environment(Environment enclosing){
+        this.enclosing = enclosing;
     }
 }
--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -52,6 +52,7 @@ class Parser {
     //statement -> exprStmt | printStmt
     private Stmt statement(){
         if(match(PRINT)) return printStatement();
+        if(match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
     }
 
@@ -80,6 +81,17 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block(){
+        List<Stmt> statements = new ArrayList<>();
+
+        while(!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block. ");
+        return statements;
     }
 
     //assignment -> IDENTIFIER "=" assignment | equality ;
