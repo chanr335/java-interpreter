77ba8c45c571a5243f68afb075a2fbea5c05a986
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-02-04 01:06:02 -0500

    Implemented init()

--- com/craftinginterpreters/lox/Resolver.java
+++ com/craftinginterpreters/lox/Resolver.java
@@ -17,9 +17,16 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     private enum FunctionType{
         NONE,
         FUNCTION,
+        INITIALIZER,
         METHOD
     }
 
+    private enum ClassType{
+        NONE,
+        CLASS
+    }
+
+    private ClassType currentClass = ClassType.NONE;
 
     @Override
     public Void visitBlockStmt(Stmt.Block stmt){
@@ -31,6 +38,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
     @Override
     public Void visitClassStmt(Stmt.Class stmt){
+        ClassType enclosingClass = currentClass;
+        currentClass = ClassType.CLASS;
+
         declare(stmt.name);
         define(stmt.name);
 
@@ -39,10 +49,15 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
         for (Stmt.Function method : stmt.methods){
             FunctionType declaration = FunctionType.METHOD;
+            if(method.name.lexeme.equals("init")){
+                declaration = FunctionType.INITIALIZER;
+            }
             resolveFunction(method, declaration);
         }
 
         endScope();
+
+        currentClass = enclosingClass;
 
         return null;
     }
@@ -83,6 +98,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         }
 
         if(stmt.value != null){
+            if (currentFunction == FunctionType.INITIALIZER){
+                Lox.error(stmt.keyword,
+                    "Can't return a value from an initializer");
+            }
             resolve(stmt.value);
         }
 
@@ -163,6 +182,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
     @Override
     public Void visitThisExpr(Expr.This expr){
+        if (currentClass == ClassType.NONE){
+            Lox.error(expr.keyword,
+                "Can't use 'this' outside of a class.");
+            return null;
+        }
+
         resolveLocal(expr, expr.keyword);
         return null;
     }
