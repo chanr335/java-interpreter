53770d714f810cb82a4323c1e9d2e2f0c70b5b77
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-02-03 22:46:06 -0500

    Beginning to implementing Classes

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -34,6 +34,7 @@ class Parser {
     //delcaration -> varDecl | statement;
     private Stmt declaration(){
         try{
+            if(match(CLASS)) return classDeclaration();
             if (match(FUN)) return function("function");
             if (match(VAR)) return varDeclaration();
             return statement();
@@ -41,6 +42,20 @@ class Parser {
         synchronize();
         return null;
         }
+    }
+
+    private Stmt classDeclaration(){
+        Token name = consume(IDENTIFIER, "Expect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+
+        List<Stmt.Function> methods = new ArrayList<>();
+        while(!check(TokenType.RIGHT_BRACE) && !isAtEnd()){
+            methods.add(function("method"));
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+
+        return new Stmt.Class(name, methods);
     }
 
     //statement -> exprStmt | forStmt | if Stmt | printStmt | whileStmt | block;
