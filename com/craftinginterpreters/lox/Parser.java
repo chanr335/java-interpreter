a2495d4517fe95747c6eb644246d70df3dd1c27b
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-02-04 01:37:35 -0500

    Adding Superclasses and Inheritance, started implementing calling superclass methods

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -46,7 +46,15 @@ class Parser {
 
     private Stmt classDeclaration(){
         Token name = consume(IDENTIFIER, "Expect class name.");
+
+        Expr.Variable superclass = null;
+        if(match(LESS)){
+            consume(IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
+
         consume(LEFT_BRACE, "Expect '{' before class body.");
+
 
         List<Stmt.Function> methods = new ArrayList<>();
         while(!check(TokenType.RIGHT_BRACE) && !isAtEnd()){
@@ -55,7 +63,7 @@ class Parser {
 
         consume(RIGHT_BRACE, "Expect '}' after class body.");
 
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     //statement -> exprStmt | forStmt | if Stmt | printStmt | whileStmt | block;
@@ -358,6 +366,14 @@ class Parser {
 
         if(match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
+        }
+
+        if(match(SUPER)){
+            Token keyword = previous();
+            consume(DOT, "Expect ',' after 'super'.");
+            Token method = consume(IDENTIFIER,
+                "Expect superclass method name.");
+            return new Expr.Super(keyword, method);
         }
 
         if(match(THIS)) return new Expr.This(previous());
